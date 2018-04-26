d47d13532390e673de282e9ed3813d1234aecc7e
DESKTOP-O9V6V35\Muhammad ilyas <ilyas>
2018-04-26 13:33:11 +0500

    reverse changes

--- app.js
+++ app.js
@@ -12,4 +12,4 @@ res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, ()=>{  console.log("App running")});
+app.listen(process.env.PORT || 5000, ()=>{  console.log("App running")});
